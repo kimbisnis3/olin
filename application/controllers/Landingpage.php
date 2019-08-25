03e622310dda0fe65868e7eda8ac49b263ad93d2
edo pratama <kimbisnis3@gmail.com>
2019-08-26 01:09:06 +0700

    update

--- application/controllers/Landingpage.php
+++ application/controllers/Landingpage.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Landingpage extends CI_Controller {
     
     public $table       = '';
-    public $judul       = 'Landing Page';
+    public $judul       = 'Dashboard';
     public $aktifgrup   = '';
     public $aktifmenu   = '';
     public $foldername  = '';
@@ -21,6 +21,33 @@ class Landingpage extends CI_Controller {
         $data['aktifgrup']  = $this->aktifgrup;
         $data['aktifmenu']  = $this->aktifmenu;
         $title      = $this->judul;
+        $q_po_pending   = "SELECT
+                            COUNT (*)
+                        FROM
+                            xorder
+                        WHERE
+                            void IS NOT TRUE
+                        AND status = 0";
+        $q_po_proses    = "SELECT
+                            COUNT (*)
+                        FROM
+                            xorder
+                        WHERE
+                            void IS NOT TRUE
+                        AND status > 0
+                        AND status < 4";
+        $q_po_ready     = "SELECT
+                            COUNT (*)
+                        FROM
+                            xorder
+                        WHERE
+                            void IS NOT TRUE
+                        AND status = 4";
+
+        $data['po_pending'] =  $this->db->query($q_po_pending)->row();
+        $data['po_proses']  =  $this->db->query($q_po_proses)->row();
+        $data['po_ready']   =  $this->db->query($q_po_ready)->row();
+        
         $this->load->view($this->indexpage, $data);  
     }
 
--- application/views/v_landingpage.php
+++ application/views/v_landingpage.php
@@ -15,9 +15,52 @@
             <li class="active title"><?php echo $title; ?></li>
           </ol>
         </section>
+        <section class="content fadeIn animated">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="small-box bg-yellow">
+                <div class="inner">
+                  <h3><?php echo $po_pending->count ?></h3>
+                  <p>PO Pending</p>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-exchange swing animated"></i>
+                </div>
+                <a href="<?php echo base_url() ?>po" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                  <h3><?php echo $po_proses->count ?></h3>
+                  <p>PO Dalam Proses Produksi</p>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-refresh"></i>
+                </div>
+                <a href="<?php echo base_url() ?>po" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="small-box bg-green">
+                <div class="inner">
+                  <h3><?php echo $po_ready->count ?></h3>
+                  <p>PO Siap Kirim</p>
+                </div>
+                <div class="icon">
+                  <i class="fa fa-check bounceIn animated"></i>
+                </div>
+                <a href="<?php echo base_url() ?>po" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
       <?php $this->load->view('_partials/foot'); ?>
     </div>
   </body>
 </html>
 <?php $this->load->view('_partials/js'); ?>
+<script type="text/javascript">
+  $(".title").html("Dashboard");
+</script>
