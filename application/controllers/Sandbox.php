6898d96a2783205c151f181cfc338c465edb1e23
edo pratama <kimbisnis3@gmail.com>
2019-08-27 17:22:27 +0700

    sandbox

--- application/controllers/Sandbox.php
+++ application/controllers/Sandbox.php
@@ -59,24 +59,22 @@ class Sandbox extends CI_Controller {
         
     }
 
-    function contoharraybanyak() {
-        $companies = array(
-           array( "label" => "JAVA", "value" => "1" ),
-           array( "label" => "DATA IMAGE PROCESSING", "value" => "2" ),
-           array( "label" => "JAVASCRIPT", "value" => "3" ),
-           array( "label" => "DATA MANAGEMENT SYSTEM", "value" => "4" ),
-           array( "label" => "COMPUTER PROGRAMMING", "value" => "5" ),
-           array( "label" => "SOFTWARE DEVELOPMENT LIFE CYCLE", "value" => "6" ),
-           array( "label" => "LEARN COMPUTER FUNDAMENTALS", "value" => "7" ),
-           array( "label" => "IMAGE PROCESSING USING JAVA", "value" => "8" ),
-           array( "label" => "CLOUD COMPUTING", "value" => "9" ),
-           array( "label" => "DATA MINING", "value" => "10" ),
-           array( "label" => "DATA WAREHOUSE", "value" => "11" ),
-           array( "label" => "E-COMMERCE", "value" => "12" ),
-           array( "label" => "DBMS", "value" => "13" ),
-           array( "label" => "HTTP", "value" => "14" )
-            
+    function contoharray() {
+        $config = array(
+           array( "header" => "ID", "body" => "id" ),
+           array( "header" => "Kode", "body" => "kode" ),
+           array( "header" => "Useri", "body" => "useri" )
         );
+        $result = $this->db->get('xorder')->result_array();
+        foreach ($config as $i => $v)  
+        {
+          echo $v['header'];
+        }
+        foreach ($result as $i => $t) {
+          foreach ($config as $i => $v) {
+            echo $t[$v['body']]."<br>";
+          }
+        }
     }
 
     function mapdir() {
