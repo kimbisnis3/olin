5fb6637f3de2e9d666d93344660a26179f64e937
edo pratama <kimbisnis3@gmail.com>
2019-11-16 11:51:51 +0700

    AGEN || query Laporan Agen

--- agen/application/controllers/laporan/Laporanpoagen.php
+++ agen/application/controllers/laporan/Laporanpoagen.php
@@ -24,14 +24,16 @@ class Laporanpoagen extends CI_Controller {
         $st   = date('Y-m-d', strtotime($this->input->post('awal')));
         $en   = date('Y-m-d', strtotime($this->input->post('akhir')));
         $q     = "SELECT
-                    xorder.kode,
                     to_char(xorder.tgl, 'DD Mon YYYY') tgl,
-                    xorder.ref_cust,
-                    mcustomer.nama agen,
-                    xorder.ref_layanan,
-                    mlayanan.nama layanan,
+                    xorder.kode,
+                    mcustomer.nama customer_nama,
+                    mbarang.nama barang_nama,
+	                mwarna.nama warna_nama,
+                    mlayanan.nama layanan_nama,
+                    xorderd.jumlah,
+                    xorderd.harga,
+                    xorder.bykirim,
                     xorder.total,
-                    xorder.status,
                     CASE xorder.status
                 WHEN 0 THEN
                     'Pending'
@@ -46,13 +48,32 @@ class Laporanpoagen extends CI_Controller {
                 WHEN 5 THEN
                     'Sudah Dikirim'
                 END AS status_nama,
-                 xorder.ket
+                mjenbayar.nama jenbayar_nama,
+                CASE xpelunasan.ref_jenbayar
+                WHEN 'GX0003' THEN
+                    xpelunasan.bayar
+                ELSE
+                    '0'
+                END AS dp,
+                CASE xpelunasan.ref_jenbayar
+                WHEN 'GX0001' THEN
+                    xpelunasan.bayar
+                ELSE
+                    '0'
+                END AS lunas,
+                xorder.ket
                 FROM
                     xorder
-                LEFT OUTER JOIN mcustomer ON mcustomer.kode = xorder.ref_cust
-                LEFT OUTER JOIN mlayanan ON mlayanan.kode = xorder.ref_layanan
-                WHERE
-                    xorder.void IS NOT TRUE";
+                LEFT JOIN xpelunasan ON xpelunasan.ref_jual = xorder.kode
+                LEFT JOIN mjenbayar ON mjenbayar.kode = xpelunasan.ref_jenbayar
+                LEFT JOIN xorderd ON xorderd.ref_order = xorder.kode
+                LEFT JOIN msatbrg ON msatbrg.kode = xorderd.ref_satbrg
+                LEFT JOIN mbarang ON mbarang.kode = msatbrg.ref_brg
+                LEFT JOIN mbarangs ON mbarang.kode = mbarangs.ref_brg
+                LEFT JOIN mwarna ON mwarna.kode = mbarangs.warna
+                LEFT JOIN mmodesign ON mmodesign.kode = mbarangs.model
+                LEFT JOIN mcustomer ON mcustomer.kode = xorder.ref_cust
+                LEFT JOIN mlayanan ON mlayanan.kode = xorder.ref_layanan";
         if ($st || $en) {
             $q  .=" AND
                     xorder.tgl 
@@ -65,12 +86,66 @@ class Laporanpoagen extends CI_Controller {
         $data['result'] = $this->db->query($q)->result_array();
         $data['periodestart'] = $this->input->post('awal');
         $data['periodeend']   = $this->input->post('akhir');
-        $data['header'] = ['Kode','Tanggal','Layanan','Total','Status','Keterangan'];
-        $data['body']   = ['kode','tgl','layanan','total','status_nama','ket'];
+        $data['header'] = ['Tanggal','Kode','Agen','Jenis Order','Warna','Layanan','QTY','Harga','Ongkos Kirim','Grand Total','Status','DP','Pelunasan','Keterangan'];
+        $data['body']   = ['tgl','kode','customer_nama','barang_nama','warna_nama','layanan_nama','jumlah','harga','bykirim','total','status_nama','dp','lunas','ket'];
         $data['maskgb'] = $this->input->post('mask-gb');
         $data['title']  = $this->titlereport;
         $data['gb']     = $this->input->post('gb');
         $this->load->view($this->printpage,$data);
     }
+
+    // function cetakx()
+    // {
+    //     $st   = date('Y-m-d', strtotime($this->input->post('awal')));
+    //     $en   = date('Y-m-d', strtotime($this->input->post('akhir')));
+    //     $q     = "SELECT
+    //                 xorder.kode,
+    //                 to_char(xorder.tgl, 'DD Mon YYYY') tgl,
+    //                 xorder.ref_cust,
+    //                 mcustomer.nama agen,
+    //                 xorder.ref_layanan,
+    //                 mlayanan.nama layanan,
+    //                 xorder.total,
+    //                 xorder.status,
+    //                 CASE xorder.status
+    //             WHEN 0 THEN
+    //                 'Pending'
+    //             WHEN 1 THEN
+    //                 'Proses Produksi'
+    //             WHEN 2 THEN
+    //                 'Proses Produksi'
+    //             WHEN 3 THEN
+    //                 'Proses Produksi'
+    //             WHEN 4 THEN
+    //                 'Ready'
+    //             WHEN 5 THEN
+    //                 'Sudah Dikirim'
+    //             END AS status_nama,
+    //              xorder.ket
+    //             FROM
+    //                 xorder
+    //             LEFT OUTER JOIN mcustomer ON mcustomer.kode = xorder.ref_cust
+    //             LEFT OUTER JOIN mlayanan ON mlayanan.kode = xorder.ref_layanan
+    //             WHERE
+    //                 xorder.void IS NOT TRUE";
+    //     if ($st || $en) {
+    //         $q  .=" AND
+    //                 xorder.tgl 
+    //             BETWEEN '$st' AND '$en'";
+    //     }
+    //     $q  .=" AND xorder.ref_cust = '{$this->session->userdata('kodecust')}'";
+    //     if ($this->input->post('gb')) {
+    //         $q .=" ORDER BY {$this->input->post('gb')}";
+    //     }
+    //     $data['result'] = $this->db->query($q)->result_array();
+    //     $data['periodestart'] = $this->input->post('awal');
+    //     $data['periodeend']   = $this->input->post('akhir');
+    //     $data['header'] = ['Kode','Tanggal','Layanan','Total','Status','Keterangan'];
+    //     $data['body']   = ['kode','tgl','layanan','total','status_nama','ket'];
+    //     $data['maskgb'] = $this->input->post('mask-gb');
+    //     $data['title']  = $this->titlereport;
+    //     $data['gb']     = $this->input->post('gb');
+    //     $this->load->view($this->printpage,$data);
+    // }
     
 }
